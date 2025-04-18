35f5b93cad06e61fab1dc8213aa09217dd650432
LauPerisse <123lautiperisse123@gmail.com>
2025-04-18 13:33:42 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
 
-typedef struct FecNac {
-    int dia;
-    int mes;
-    int anio;
-} tFecNac;
-
-typedef structu Socio {
-    int numeroDeSocio;
-    int DNI;
-    char[N] apellido;
-    char[N] nombre;
-    char[M] domicilio;
-    tFecNac fecNac;
-    tFecNac fecAsociasion;
-} tSocio;
-
 int main() {
     printf("Naim gordo");
     
