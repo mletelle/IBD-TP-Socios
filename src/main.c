0448e14119faaed229c7928d9c0ad9c912dc6d41
LauPerisse <123lautiperisse123@gmail.com>
2025-04-18 13:18:25 -0300

    Update 1 main.c
    se creo la estructura de socio

--- src/main.c
+++ src/main.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+
+typedef struct tFecNac {
+    int dia;
+    int mes;
+    int anio;
+} FecNac;
+
+typedef structu tSocio {
+    int numeroDeSocio;
+    int DNI;
+    char[N] apellido;
+    char[N] nombre;
+    char[M] domicilio;
+    FecNac fecNac;
+    int fecAsociasion;
+} Socio;
+
 int main() {
     printf("Naim gordo");
     
