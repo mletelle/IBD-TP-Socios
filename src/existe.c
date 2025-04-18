4ebef19d4586d788e4bdde1e3996905352c23e0d
NaimBravo <naimbravo1@gmail.com>
2025-04-18 15:45:21 -0300

    Primera versión de existe.c

---
+++ src/existe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include "socio.h"
+int existe(FILE *archivo, int nro_buscar){
+    tSocio socio;
+    int encontrado = 0;
+    fseek(archivo, 0, SEEK_SET); // Mover el puntero al inicio del archivo
+    while ((fread(&socio, sizeof(tSocio), 1, archivo) == 1) && (!encontrado)) {
+        if (socio.numeroDeSocio == nro_buscar) {
+            encontrado = 1;
+        }
+    }
+    return encontrado;
+}
